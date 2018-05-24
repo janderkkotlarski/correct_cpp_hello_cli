14d30d7375a7109d52e3ce64eb3bdc9330f49f86
janderkkotlarski <j.d.kotlarski@gmx.com>
2018-05-24 22:00:16 +0200

    may it pass...

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
-main(argc, argv) 
-{
+#include <iostream>
+#include <cassert>
 
+/// main with command line input
+int main(int argc, char *argv[])
+{
+    assert(argc > 0); /// Test whether argc is 1 or more.
+
+    std::cout << "Hello";
+
+    if (argc > 1)
+    { std::cout << ' ' << argv[1]; }
+
+    std::cout << '\n';
 }
